7fbf0e0d1f0116afa2192a0a969b5099eccc1e4d
Afsal.MS <afsal.ms@iwaresolutions.net>
2024-11-12 20:39:51 +0530

    Add project files.

---
+++ HrLeaveManagement.Domain/LeaveRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrLeaveManagement.Domain
+{
+    public class LeaveRequest
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public DateTime DateRequested { get; set; }
+        public string RequestComment { get; set; }
+        public DateTime DateActioned { get; set; }
+        public bool? Approved { get; set; }
+        public bool Cancelled { get; set; }
+
+    }
+}
